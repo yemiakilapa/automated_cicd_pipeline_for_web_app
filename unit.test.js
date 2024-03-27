7d964e2fdda7e893f82400249ba7249b3de8fccf
yemiakilapa <yemiakilapa@gmail.com>
2024-03-27 11:57:13 +0000

    update

--- unit.test.js
+++ unit.test.js
@@ -1,8 +1,13 @@
 
-const { add } = require('./utility'); 
+test('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(3);
+});
 
+
+function add(a, b) {
+  return a + b;
+}
 
 test('add function adds two numbers correctly', () => {
-  const result = add(2, 3);
-  expect(result).toBe(5);
+  expect(add(2, 3)).toBe(5);
 });
